64cea13d31fca95c2277a98954175baa614a403d
AlexDrags <vera2052@mail.ru>
2022-05-16 14:49:26 +0300

    Добавил скрипт появления модалок если не заполнен телефон и почта

---
+++ source/js/modal.js
@@ -0,0 +1,31 @@
+let tel = document.querySelector('.person-data__input--third--tel');
+let mail = document.querySelector('.person-data__input--third--mail');
+let visibled = document.querySelector ('.modal--visibled');
+let failModal = document.querySelector('.modal--invalid');
+let form = document.querySelector('.form');
+let allOk = document.querySelector('.modal--success');
+let modal = document.querySelector('.modal');
+let modalOne = document.querySelector('.button--first--js');
+let modalTwo = document.querySelector('.button--second--js');
+
+form.onsubmit = function(evt) {
+  evt.preventDefault();
+
+
+  if ( mail.value == 'Введите почту *' || tel.value == 'Номер, пожалуйста') {
+    allOk.classList.remove('modal--visibled');
+    failModal.classList.add('modal--visibled');
+  } else {
+    failModal.classList.remove('modal--visibled');
+    allOk.classList.add('modal--visibled');
+  }
+
+};
+
+modalOne.onclick = function() {
+  failModal.classList.remove('modal--visibled');
+};
+
+modalTwo.onclick = function() {
+  allOk.classList.remove('modal--visibled');
+};
